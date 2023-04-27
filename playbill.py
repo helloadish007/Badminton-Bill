67e597fe3bb5a0c1435a79046d240d2dd634faf5
ADISH.A.P <helloadish007@gmail.com>
2023-04-27 21:09:51 +0530

    Update playbill.py

--- playbill.py
+++ playbill.py
@@ -31,7 +31,6 @@ regular_player_list=['Adish','Vasu bro','Bala','Karthik','Reymon','Abhishek','An
 if st.button('Want to Add New Player?'):
      new_player = st.text_input('Enter the name of new player:  ', '')
      players_list.append(new_player)
-     break
      
 total = st.text_input('Total Cost:  ', '')
 hours = st.slider('Number of hours played', 1, 3, 2)
